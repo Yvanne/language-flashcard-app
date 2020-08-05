084affab493a80ebbb9902fbf6f4ffc24c7f0727
Yvanne Seuhou <yseuhou@umd.edu>
2020-08-05 02:00:29 -0400

    displaying random words

--- app/src/main/java/com/example/flashcard_app/MainActivity.kt
+++ app/src/main/java/com/example/flashcard_app/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.flashcard_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val wordList = arrayListOf<String>("boca", "arroz","pagar", "tienda",
+        "mesa", "gato", "cerveza", "dulces", "cielos")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            wordBtn.setOnClickListener {
+                val random = Random()
+                val randomWord = random.nextInt(wordList.count())
+                selectedWord.text = wordList[randomWord]
+            }
     }
 }
