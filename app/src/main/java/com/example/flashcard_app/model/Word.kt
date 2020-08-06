19cf4ea6ef07001fe551e8abb10677f8918befc8
Dennis McDaid <dennismcdaid@192-168-1-109.tpgi.com.au>
2020-08-06 13:09:32 +1000

    Add random word card and text input.

---
+++ app/src/main/java/com/example/flashcard_app/model/Word.kt
@@ -0,0 +1,18 @@
+package com.example.flashcard_app.model
+
+data class Word(
+    var english: String,
+    val translation: String,
+    val language: Language
+)
+
+
+val exampleWords = listOf(
+    Word("Dog", "Perro", Language.SPANISH),
+    Word("Rice", "Arroz", Language.SPANISH),
+    Word("Hello", "Hola", Language.SPANISH),
+    Word("Sugar", "Azucar", Language.SPANISH)
+)
+
+val wordList = arrayListOf<String>("boca", "arroz","pagar", "tienda",
+    "mesa", "gato", "cerveza", "dulces", "cielos")
